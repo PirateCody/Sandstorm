7cb9a77de425e133d92fd14e2deca286cf03d92c
PirateCody <piratecody16@gmail.com>
2015-08-22 18:28:59 -0500

    fix

--- src/com/piratecody/sandstorm/blocks/SSBlock.java
+++ src/com/piratecody/sandstorm/blocks/SSBlock.java
@@ -1,7 +1,6 @@
 package com.piratecody.sandstorm.blocks;
 
 import com.piratecody.sandstorm.Reference;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
@@ -10,10 +9,16 @@ public class SSBlock extends Block
 {
     public SSBlock(String name)
     {
-        super(Material.rock);
+        super(Material.sand);
         this.setCreativeTab(CreativeTabs.tabBlock);
         this.setBlockName(name);
         this.setBlockTextureName(Reference.MODID + ":"+ name);
+        this.setHardness(0.7F);
+        this.setResistance(0.5F);
+        
        
     }
+    
+   
+
 }
